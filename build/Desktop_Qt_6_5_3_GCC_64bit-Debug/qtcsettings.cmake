6b1d28cb1a07efc27b9f886f743673c8a8555299
hajihasani <hajihassany18@gmail.com>
2025-08-23 00:30:12 +0330

    edit cmake list

--- build/Desktop_Qt_6_5_3_GCC_64bit-Debug/qtcsettings.cmake
+++ build/Desktop_Qt_6_5_3_GCC_64bit-Debug/qtcsettings.cmake
@@ -1,10 +1,10 @@
 # This file is managed by Qt Creator, do not edit!
 
 set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/asus/proj/qt/sta309a_audio_card/build/Desktop_Qt_6_5_3_GCC_64bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
 set("CMAKE_PREFIX_PATH" "/home/asus/Qt/6.5.3/gcc_64" CACHE "PATH" "" FORCE)
 set("CMAKE_CXX_COMPILER" "/usr/bin/clang++-18" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/asus/proj/qt/sta309a_audio_card/build/Desktop_Qt_6_5_3_GCC_64bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
 set("CMAKE_C_COMPILER" "/usr/bin/clang-18" CACHE "FILEPATH" "" FORCE)
 set("QT_QMAKE_EXECUTABLE" "/home/asus/Qt/6.5.3/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
